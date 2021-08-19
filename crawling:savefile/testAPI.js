7752e266a482d5269627ed741fa5464c752e87db
yth1122 <yxk1212@naver.com>
2021-08-19 12:08:19 +0900

    crawling후 txt파일로 다운

---
+++ crawling:savefile/testAPI.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+const axios = require("axios");
+const cheerio = require("cheerio");
+var fs = require('fs');
+router.get('/',async function(req,res){
+	const getHtml = async () => {
+		try {
+		  return await axios.get("http://egloos.zum.com/metalbear/v/3521684");
+		} catch (error) {
+		  console.error(error);
+		}
+	  };
+	  getHtml()
+	  .then(async html => {
+		let ulList = [];
+			let array = [];
+			const $ = cheerio.load(html.data);
+		ulList=$("div.post");
+		   for(let i=0; i<ulList[0].children.length;i++){
+				if(ulList[0].children[i].data){
+			  array[i]=ulList[0].children[i].data;
+				}	
+		   }
+			 let data = await array.toString();
+			 data=data.split(',,').toString();
+			 fs.writeFile('mysql.txt',data,(err,data)=>{
+					if(err){
+						console.log(err)
+					}else{
+						console.log('download success');
+					}
+			 })
+			 res.send(data);	
+			})
+		 
+})
+
+
+
+  module.exports= router;
