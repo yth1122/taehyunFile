290f390a1e79d8f637f2eca052e9396c2f20bfde
yth1122 <74342126+yth1122@users.noreply.github.com>
2021-08-19 12:26:31 +0900

    Update testAPI.js

--- crawling:savefile/testAPI.js
+++ crawling:savefile/testAPI.js
@@ -14,25 +14,25 @@ router.get('/',async function(req,res){
 	  getHtml()
 	  .then(async html => {
 		let ulList = [];
-			let array = [];
-			const $ = cheerio.load(html.data);
+		let array = [];
+		const $ = cheerio.load(html.data);
 		ulList=$("div.post");
-		   for(let i=0; i<ulList[0].children.length;i++){
-				if(ulList[0].children[i].data){
-			  array[i]=ulList[0].children[i].data;
-				}	
-		   }
-			 let data = await array.toString();
-			 data=data.split(',,').toString();
-			 fs.writeFile('mysql.txt',data,(err,data)=>{
-					if(err){
-						console.log(err)
-					}else{
-						console.log('download success');
-					}
-			 })
-			 res.send(data);	
-			})
+		for(let i=0; i<ulList[0].children.length;i++){
+			if(ulList[0].children[i].data){
+				array[i]=ulList[0].children[i].data;
+			}	
+		}
+		let data = await array.toString();
+		data=data.split(',,').toString();
+		fs.writeFile('mysql.txt',data,(err,data)=>{
+			if(err){
+				console.log(err)
+			}else{
+				console.log('download success');
+			}
+		})
+		res.send(data);	
+	})
 		 
 })
 
