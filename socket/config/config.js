5758e18fe12195ed42eff14ed2f32539a9db5bfd
yth1122 <74342126+yth1122@users.noreply.github.com>
2021-09-30 17:08:07 +0900

    Delete config.js

--- socket/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'kakaoID' : "f485ceebfea86e131a11e2019a5281a4",
-    'facebookID' : "273494967956069",
-    'facebookSecret': "e85398ec5df66547097d1eeb5a39acff",
-    'naverID':"dYPlxt4UhWT90fcE0f1T",
-    'naverSecret':"6UK2W6xHwd",
-    'googleID':"849753746495-cqaffccrg66aodgc09m0nqlncikskums.apps.googleusercontent.com",
-    'googleSecret':"r9e3b-U6FcCa_VKDrCCH7334"
-}
