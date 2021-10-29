465596cea172b3ba53cd78c3c9d5d0ab893c58d6
yth1122 <yxk1212@naver.com>
2021-10-29 17:32:21 +0900

    modi

--- android/monitor/App.vue
+++ android/monitor/App.vue
@@ -1,25 +1,126 @@
 <template>
-  <web-view :source="{uri:'https://mon-dot-centered-sight-237801.an.r.appspot.com/'}" 
-            :style="{marginTop:25}"/>
+  <view class='container'>
+    <view class='container' >
+      <status-bar
+            background-color="white"
+            color="black"
+            bar-style="dark-content"
+        />
+      <!-- <button 
+        :on-press="onPressLearnMore"
+        title="BaroFactory"
+        color='black'
+        ></button> -->
+         <ActionBar
+          containerStyle='styles.bar'
+          backgroundColor="black"
+          leftIconName='menu'
+          :onLeftPress="open"
+          :flat="true"
+          :rightIcons="rightIcons" 
+          :disableStatusBarHandling="false"
+          />
+          
+          <!-- <DrawerLayoutAndroid v-if='bool' ref='drawer' 
+            :drawerWidth="300"
+            drawerPostion="Left"
+          >
+          <sidemenu render-prop-fn="rederNavigationView"></sidemenu>
+          <slot></slot>
+          </DrawerLayoutAndroid> -->
+          <test/>
+
+
+         <!-- <text class='title'>Android so difficult</text>  -->
+          <web-view useWebKit={true}
+            :source="{
+              uri:url,
+            }" 
+          />
+    </view>
+  </view>  
 </template>
 <script>
+import React,{useRef , useState} from "react";
 import { WebView } from "react-native-webview";
-
+import { StyleSheet,Button,RefreshControl, DrawerLayoutAndroid, Text, View ,Alert } from 'react-native';
+import ActionBar from 'react-native-action-bar';
+import sidemenu from './src/SideMenu.vue';
+import test from './src/test.js';
+const navigationView = sidemenu;
 export default {
   name: "myComponent",
+  data(){
+    return{
+      bool:false,
+      url:'https://mon-dot-centered-sight-237801.an.r.appspot.com/',
+      searchQuery:null,
+      navigation:sidemenu,
+      rightIcons:[
+         {
+            name: 'star',
+            onPress: () => alert('Right Star !'),
+        },
+        {
+            name: 'phone',
+            onPress: () => alert('Right Phone !'),
+            isBadgeLeft: true,
+        },
+        {
+            name: 'plus',
+            onPress: () => alert('Right Plus !'),
+        },
+        {
+            name: 'flag',
+            badge: '10',
+            onPress: () => alert('Right Flag !'),
+        },
+      ]
+    }
+  }, 
   components: {
-    "web-view": WebView
+    "web-view": WebView,
+    "button" :Button,
+    "RefreshControl" : RefreshControl,
+    "ActionBar" : ActionBar,
+    "sidemenu" : sidemenu,
+    "DrawerLayoutAndroid":DrawerLayoutAndroid,
+    "navigationView":navigationView,
+    "test":test
+  },
+  methods:{
+    onPressLearnMore: function() {
+      this.bool = true;
+      // Alert.alert('hi');
+    },
+    open(){
+      this.bool=true;
+    },
+    // onRefresh(){
+    //   this.bool=true;
+    //   this.bool=false;
+    // },
+  },
+  mounted:function(){
   }
 };
 </script>
 <style>
+.title {
+  text-align:center;
+  height:3%;
+}
 .container {
   background-color: white;
-  align-items: center;
   justify-content: center;
   flex: 1;
+  width:100%;
+
+}
+.action{
 }
 .text-color-primary {
   color: blue;
 }
+
 </style>
---
+++ android/monitor/src/SideMenu.vue
@@ -0,0 +1,119 @@
+<template>
+    <view
+      :style="{borderTopRightRadius: 50,
+        borderBottomRightRadius: 50,
+        backgroundColor: 'white',
+        flex: 1}"
+    >
+      <view
+        :style="{
+          height: 200,
+          backgroundColor: 'rgba(213,213,213,0.27)',
+          borderTopRightRadius: 50,
+          paddingTop: 50,
+          paddingLeft: 30,}
+      ">
+        <image
+          :style="{height: 72, width: 72, borderRadius: 37}"  
+          :source="{uri: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png' }" 
+        />
+        <text
+          :style="{
+            marginTop: 15,
+            fontSize: 26,
+            fontWeight: 'bold',
+            fontStyle: 'normal',
+            lineHeight: 31,
+            letterSpacing: 0,
+            textAlign: 'left',
+            color: '#272727',
+            }">
+          airplane
+        </text>
+      </view>
+      <view>
+          <view v-for="data in list"
+								:key="data.index"
+					>
+            <touchable-opacity
+              :style="{
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingVertical: 5,
+                paddingTop: 27,
+                paddingLeft: 30,
+                }"
+            :onPress="sex">
+              <text
+                :style="{
+                  marginLeft: 12,
+                  color: '#272727',
+                  fontSize: 16,
+                  lineHeight: 19,
+                }">
+                {{data.title}}
+              </text>
+            </touchable-opacity>
+          </view>
+      </view>
+    </view>
+</template>
+<script>
+export default {
+  name: "myComponent",
+  data(){
+    return{
+			list: [
+        {
+          idx: 0,
+          title: '소개',
+        },
+        {
+          idx: 1,
+          title: '공지사항',
+        },
+        {
+          idx: 2,
+          title: '정보',
+        },
+        {
+          idx: 3,
+          title: '뉴스',
+        },
+        {
+          idx: 4,
+          title: '포인트',
+        },
+        {
+          idx: 5,
+          title: '초대',
+        },
+        {
+          idx: 6,
+          title: '고객센터',
+        },
+        {
+          idx: 7,
+          title: '설정',
+        },
+        {
+          idx: 8,
+          title: '로그아웃',
+        },
+			]
+		}
+  },
+  components: {
+  },
+  methods:{
+		sex(){
+			alert('fuck');
+		}
+  },
+  mounted:function(){
+  }
+};
+</script>
+<style>
+
+</style>
